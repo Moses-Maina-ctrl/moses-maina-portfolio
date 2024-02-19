551e7daf4012b5922a7c7a7e0c731f124c6d370d
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-02-20 00:24:50 +0300

    About me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         'nav-bar': '#C4C4C4',
         'mainHero': '#EBEBEB',
         'nav-grey': '#DFE9F3',
+        'pastel-grey': '#c2c2c2'
       },
+    },
+    fontFamily: {
+      honk: ['Honk', 'system-ui']
     },
   },
   plugins: [],
