00da7c84de04a65706866fa511f4e65f6f360eaa
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-02-21 00:02:47 +0300

    About me section

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu");
+const menuItems = document.querySelector("#menu-items");
+
+menu.addEventListener('click', () => {
+  menuItems.classList.toggle("hidden");
+
+})
