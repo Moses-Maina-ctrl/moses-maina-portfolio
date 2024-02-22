978393093bce14aabef40639e60c9bc79e86e203
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-02-22 13:07:02 +0300

    About me section

---
+++ typing.js
@@ -0,0 +1,11 @@
+const typing = new AutoTyping('#text', [
+  'Software Engineering',
+  'Graduate Civil Engineer',
+], {
+  typeSpeed: 50,
+  deleteSpeed: 50,
+  waitBeforeDelete: 2000,
+  waitBetweenWords: 500,
+});
+
+typing.start();
